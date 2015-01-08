4221b4516c93e3f37418e2ec9df4d0c1cdb1d1eb
dsean <dsean@kimo.com>
2015-01-08 10:11:59 +0800

    Revert "Feature/calendar ui"

--- DSCalendar/ViewController.m
+++ DSCalendar/ViewController.m
@@ -7,11 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "DSCalendarKit/DSCalendarView.h"
 
-@interface ViewController () <DSCalendarViewDelegate>
-
-@property (strong, nonatomic) DSCalendarView *calendarView;
+@interface ViewController ()
 
 @end
 
@@ -20,28 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.calendarView = [[DSCalendarView alloc] init];
-    [self.DScalendarView addSubview:self.calendarView];
-
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    self.calendarView.delegate = self;
-    [self.calendarView setCalendarType:@"month"];
-    [self.calendarView reloadUI];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    self.calendarView.delegate = nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)didSeletedDate {
-    [self performSegueWithIdentifier:@"weekSegue" sender:self];
 }
 
 @end
